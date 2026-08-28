4ef4c1d5a374c0bbeec7f1216cbe5333bed0c9f9
Junseo Lee <junseolee9@gmail.com>
2026-08-27 23:05:27 -0700

    Update submission-3.py

--- Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.py
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.py
@@ -3,7 +3,7 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[j] > nums[i]:
-                    i, j = i+1, j+1
+                    continue
                 else:
                     return nums[j]
         return nums[0]
